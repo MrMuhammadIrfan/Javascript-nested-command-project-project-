0a198d3cbaa79d3c1b11530e83a4111327498f93
unknown <muhammadirfansce@gmail.com>
2024-02-24 12:53:45 +0500

    Project Initialized and 50% work completed

---
+++ script.js
@@ -0,0 +1,4 @@
+const user = prompt("Enter The Item You Want To 1.Shirt 2.Pent 3.Trouser 4.Shawl")
+if(user==1){
+    const shirt =prompt("Please mention the size of the Shirt Which you want to wear")
+}
